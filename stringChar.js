358a62c46a1a66967b66998b1da70d40ece4d8ec
EbenAngeline <ebenangeline6@gmail.com>
2026-04-18 12:05:53 -0500

    Fixed  the unicode message

---
+++ stringChar.js
@@ -0,0 +1,13 @@
+
+let todoList = 
+  "To-Do list:\n-\tTask\tStatus\n-\tClean\tDone\n-\tCode\tIn Progress\n-\tRelax\tPending";
+console.log(todoList);
+
+let menuItems = "Menu:\nPizza\t\ud83c\udf55\ufe0f\nBurger\t\u{1F354}\nSushi\t\u{1F363}";
+console.log(menuItems);
+let  greet = "Greetings:\nHello\t(English)\nHola\t(spanish)\n\u3053\u3093\u306B\u3061\u306F(Japanese)";
+console.log(greet);
+let favoriteSymbol= "My Favorite Symbols:\n1.\tHeart:\u2764\n2.\tStar:\u2B50\n3.\tCheckmark:\u2713";
+console.log(favoriteSymbol);
+let receipt = "Receipt:\nBook\tPrice\n1984\t$9.99\nDune\t$14.99\nTotal\t$24.98";
+console.log(receipt);
